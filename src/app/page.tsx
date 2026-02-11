dcd98f5f8d7a2a08e56a3422a2b7e446918d763d
Mumurilo375 <murilinho.pequeno@gmail.com>
2026-02-11 19:02:07 -0300

    teste renderizar imagens do uploadthings

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,25 @@
 import Link from "next/link";
 
+const mockUrls = [
+  "https://wop051lmme.ufs.sh/f/OPsIdbYoKx9ZjJkLU35oGgHOkAyemLFWaBDx6Pw3SR2Tfzrn",
+];
+
+const mockImages = mockUrls.map((url, index) => ({
+  id: index +1,
+  url,
+}));
+
 export default function HomePage() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
-        Hello
+    <main className="">
+      <div className="flex flex-wrap">{
+        mockImages.map((image) => (
+          <div key={image.id} className="w-1/2 p-4">
+            <img src={image.url} />
+          </div>
+        ))
+        }
+      </div>
     </main>
   );
 }
