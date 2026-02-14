8e2e6a491424c7fab761ae11dc0942964c2b9ba3
Mumurilo375 <murilinho.pequeno@gmail.com>
2026-02-14 11:35:02 -0300

    consumindo tabela images na pagina

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,51 +1,23 @@
 /*import Image from "next/image";*/
 
 import { db } from "~/server/db";
-export const dynamic = "force-dynamic";
-
-const mockUrls = [
-{url:"https://wop051lmme.ufs.sh/f/OPsIdbYoKx9ZjJkLU35oGgHOkAyemLFWaBDx6Pw3SR2Tfzrn"},
-{url: "https://wop051lmme.ufs.sh/f/OPsIdbYoKx9ZuWBmchSobtdQY47URKzX0MaICAkNv3PmF58g"},
-{url:"https://wop051lmme.ufs.sh/f/OPsIdbYoKx9ZjJkLU35oGgHOkAyemLFWaBDx6Pw3SR2Tfzrn"},
-{url: "https://wop051lmme.ufs.sh/f/OPsIdbYoKx9ZuWBmchSobtdQY47URKzX0MaICAkNv3PmF58g"},
-{url:"https://wop051lmme.ufs.sh/f/OPsIdbYoKx9ZjJkLU35oGgHOkAyemLFWaBDx6Pw3SR2Tfzrn"},
-{url: "https://wop051lmme.ufs.sh/f/OPsIdbYoKx9ZuWBmchSobtdQY47URKzX0MaICAkNv3PmF58g"},
-{url:"https://wop051lmme.ufs.sh/f/OPsIdbYoKx9ZjJkLU35oGgHOkAyemLFWaBDx6Pw3SR2Tfzrn"},
-{url: "https://wop051lmme.ufs.sh/f/OPsIdbYoKx9ZuWBmchSobtdQY47URKzX0MaICAkNv3PmF58g"},
-{url:"https://wop051lmme.ufs.sh/f/OPsIdbYoKx9ZjJkLU35oGgHOkAyemLFWaBDx6Pw3SR2Tfzrn"},
-{url: "https://wop051lmme.ufs.sh/f/OPsIdbYoKx9ZuWBmchSobtdQY47URKzX0MaICAkNv3PmF58g"},
-{url:"https://wop051lmme.ufs.sh/f/OPsIdbYoKx9ZjJkLU35oGgHOkAyemLFWaBDx6Pw3SR2Tfzrn"},
-{url: "https://wop051lmme.ufs.sh/f/OPsIdbYoKx9ZuWBmchSobtdQY47URKzX0MaICAkNv3PmF58g"},
-{url:"https://wop051lmme.ufs.sh/f/OPsIdbYoKx9ZjJkLU35oGgHOkAyemLFWaBDx6Pw3SR2Tfzrn"},
-{url: "https://wop051lmme.ufs.sh/f/OPsIdbYoKx9ZuWBmchSobtdQY47URKzX0MaICAkNv3PmF58g"},
-{url:"https://wop051lmme.ufs.sh/f/OPsIdbYoKx9ZjJkLU35oGgHOkAyemLFWaBDx6Pw3SR2Tfzrn"},
-{url: "https://wop051lmme.ufs.sh/f/OPsIdbYoKx9ZuWBmchSobtdQY47URKzX0MaICAkNv3PmF58g"},
-];
-
-const mockImages = mockUrls.map((data, index) => ({
-  id: index + 1,
-  url: data.url
-}));
+export const dynamic = "force-dynamic"; // nao deixa a pagina guardar cache
 
 export default async function homePage() {
 
-  const posts = await db.query.posts.findMany();
-
-  console.log(posts);
+  const images = await db.query.images.findMany();
 
   return (
     <div className="flex flex-wrap gap-4">
-      
-      {posts.map((post) => (
-          <div key={post.id}>{post.name}</div>
-      ))}
-
-
-      {mockImages.map((image, index) => (
-          <div key={image.id + "-" + index} className="w-56">
-            <img src={image.url} alt={"image.id"}/>
+      <div className="grid grid-cols-2 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 mx-auto w-full max-w-4xl overflow-hidden">
+        {images.map((image, index) => (
+        <div key={image.id + "-" + index} className="w-full px-2" >
+          <img src={image.url} alt={"image.id"} className="w-full h-auto object-cover rounded-lg"/>
+          <div>
+            <h3>{image.name}</h3>
           </div>
-        ))}
+        </div>
+      ))}</div>
     </div>
   );
 };
