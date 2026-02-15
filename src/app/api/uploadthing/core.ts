ca80965c1f32e4db118b740a95a0a0260faef5c5
Mumurilo375 <murilinho.pequeno@gmail.com>
2026-02-15 16:25:26 -0300

    change maxfilesize to 8

--- src/app/api/uploadthing/core.ts
+++ src/app/api/uploadthing/core.ts
@@ -16,7 +16,7 @@ export const ourFileRouter = {
        * @see https://docs.uploadthing.com/file-routes#route-config
        */
       maxFileSize: "8MB",
-      maxFileCount: 1,
+      maxFileCount: 8,
     },
   })
     // Set permissions and file types for this FileRoute
